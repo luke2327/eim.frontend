fc568035bcd40781039e0785015654b1f48c135f
luke2327 <qwepoi8526@gmail.com>
2019-09-07 22:15:00 +0900

    인덴트 수정, 헤더 푸터 생성

---
+++ src/components/header/header.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+class HeaderCpt extends Component {
+  render () {
+    return (
+      <div>
+        <Link to="/enhance/enhanceEquip">
+          <div className="btn">
+            강화로 이동
+          </div>
+        </Link>
+        <Link to="/youtube_list/youtubeList">
+          <div className="btn">
+            유튜브 리스트로 이동
+          </div>
+        </Link>
+      </div>
+    )
+  }
+}
+
+export default HeaderCpt;
