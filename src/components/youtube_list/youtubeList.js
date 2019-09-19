521b21c4cd0bc6d134c6d7013e4fb5a1b278115f
luke2327 <qwepoi8526@gmail.com>
2019-09-19 10:15:14 +0900

    데코레이터 추가

--- src/components/youtube_list/youtubeList.js
+++ src/components/youtube_list/youtubeList.js
@@ -1,10 +1,27 @@
 import React, { Component } from 'react';
+import { observable, action } from 'mobx';
+import { observer } from 'mobx-react';
 
+@observer
 class YoutubeListCpt extends Component {
+  @observable number = 0;
+
+  @action
+  increase = () => {
+    this.number++;
+  }
+
+  @action
+  decrease = () => {
+    this.number--;
+  }
+
   render() {
     return (
       <div>
-        this works!!
+        <h1>{this.number}</h1>
+        <button onClick={this.increase}>+1</button>
+        <button onClick={this.decrease}>-1</button>
       </div>
     );
   }
