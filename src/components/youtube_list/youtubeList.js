00ef45ef04f5a943896dd8eecf873c62f875cc66
luke2327 <qwepoi8526@gmail.com>
2019-09-20 17:41:11 +0900

    수정

--- src/components/youtube_list/youtubeList.js
+++ src/components/youtube_list/youtubeList.js
@@ -1,9 +1,22 @@
 import React, { Component } from 'react';
 import { observer, inject } from 'mobx-react';
+import api from 'libs/api/common';
+// const api = require('libs/api/common');
 
 @inject('crawling')
 @observer
 class YoutubeListCpt extends Component {
+  componentDidMount() {
+    this.getYoutubeList('api/vod/youtube/list');
+  }
+
+  getYoutubeList = async (url) => {
+    api.send(url)
+      .then((res) => {
+        console.log(res);
+      });
+  }
+
   render() {
     return (
       <div>
---
+++ src/libs/api/common.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+export default {
+  send: (url, req, type = 'post') => {
+    const defaultUrl = 'http://localhost:6050/';
+    url = defaultUrl + url;
+    return type === 'post'
+      ? axios.post(url, req)
+      : axios.get(url, req);
+  },
+};
