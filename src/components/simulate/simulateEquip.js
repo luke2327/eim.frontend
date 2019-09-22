e031cbf596f71e58311145286bc7f29ad367f073
luke2327 <qwepoi8526@gmail.com>
2019-09-23 03:03:10 +0900

    새로고침 해도 언어 기억

--- src/components/simulate/simulateEquip.js
+++ src/components/simulate/simulateEquip.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 export default class SimulateEquipCpt extends Component {
   render() {
     return (
-      <div>
+      <div id="simulate">
         this works!
       </div>
     );
