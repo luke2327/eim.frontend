b3babd76e92ceef176528fb450be17835eb458e6
luke2327 <luke2327@luke2327ui-MacBookPro.local>
2019-09-25 08:44:05 +0900

    API 함수 명명

--- src/libs/api/maple.js
+++ src/libs/api/maple.js
@@ -1,8 +1,39 @@
-// import axios from 'axios';
 import api from 'libs/api/common';
 
 export default {
   getMapleItem: async (req) => {
     return await api.send('api/maple/item', req);
+  },
+
+  getMapleItemList: async (req) => {
+    return await api.send('api/maple/item/list', req);
+  },
+
+  getMapleItemCategory: async (req) => {
+    return await api.send('api/maple/item/category', req);
+  },
+
+  getMapleItemOverallCategory: async (req) => {
+    return await api.send('api/maple/item/category/overall-category', req);
+  },
+
+  getMapleItemBulk: async (req) => {
+    return await api.send('api/maple/item/bulk', req);
+  },
+
+  getMapleItemDetail: async (req) => {
+    return await api.send('api/maple/item/detail', req);
+  },
+
+  getMapleItemIcon: async (req) => {
+    return await api.send('api/maple/item/icon', req);
+  },
+
+  getMapleItemIconRaw: async (req) => {
+    return await api.send('api/maple/item/icon-raw', req);
+  },
+
+  getMapleItemName: async (req) => {
+    return await api.send('api/maple/item/name', req);
   },
 };
