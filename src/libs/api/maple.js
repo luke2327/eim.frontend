dbc78205cbb4608c3a79cda75538df1af74181da
luke2327 <qwepoi8526@gmail.com>
2019-09-25 09:22:59 +0900

    lodash 추가 locale 없을 경우 대비

--- src/libs/api/maple.js
+++ src/libs/api/maple.js
@@ -1,39 +1,64 @@
 import api from 'libs/api/common';
+import _ from 'lodash';
+
+const getLocale = (data) => {
+  data.locale = localStorage.getItem('language') || navigator.language.split(/[-_]/)[0] || 'en';
+
+  return data;
+};
 
 export default {
   getMapleItem: async (req) => {
+    req = req.locale ? req : getLocale(req);
+
     return await api.send('api/maple/item', req);
   },
 
   getMapleItemList: async (req) => {
+    req = req.locale ? req : getLocale(req);
+
     return await api.send('api/maple/item/list', req);
   },
 
   getMapleItemCategory: async (req) => {
+    req = req.locale ? req : getLocale(req);
+
     return await api.send('api/maple/item/category', req);
   },
 
   getMapleItemOverallCategory: async (req) => {
+    req = req.locale ? req : getLocale(req);
+
     return await api.send('api/maple/item/category/overall-category', req);
   },
 
   getMapleItemBulk: async (req) => {
+    req = req.locale ? req : getLocale(req);
+
     return await api.send('api/maple/item/bulk', req);
   },
 
   getMapleItemDetail: async (req) => {
+    req = req.locale ? req : getLocale(req);
+
     return await api.send('api/maple/item/detail', req);
   },
 
   getMapleItemIcon: async (req) => {
+    req = req.locale ? req : getLocale(req);
+
     return await api.send('api/maple/item/icon', req);
   },
 
   getMapleItemIconRaw: async (req) => {
+    req = req.locale ? req : getLocale(req);
+
     return await api.send('api/maple/item/icon-raw', req);
   },
 
   getMapleItemName: async (req) => {
+    req = req.locale ? req : getLocale(req);
+
     return await api.send('api/maple/item/name', req);
   },
 };
