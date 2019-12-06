5053dadd066db0ffd1fd335a6a26fd222ed1732a
luke2327 <qwepoi8526@gmail.com>
2019-12-06 16:37:33 +0900

    vod list 리뉴얼

--- src/stores/crawlingStore.ts
+++ src/stores/crawlingStore.ts
@@ -1,8 +1,9 @@
 import { observable } from 'mobx';
 import moment from 'moment';
+import { VodItem } from '../models/vod.interface';
 
 export default class crawlingStore {
   @observable endDate = moment();
 
-  @observable youtubeList: string[] = [];
+  @observable youtubeList: VodItem[] | undefined;
 }
